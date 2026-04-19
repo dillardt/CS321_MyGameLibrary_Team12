1069a5be80fcb7a14a262205fc7632e45b914ace
Srijal <dongolsrijal@gmail.com>
2026-04-19 15:57:35 -0500

    Model: UserDatabase class: Constructor and some methods

--- src/Model/UserDatabase.java
+++ src/Model/UserDatabase.java
@@ -7,19 +7,24 @@ import java.util.List;
 public class UserDatabase {
 
     private List<User> users;
-    private String fileLocation;
+    private final String fileLocation;
 
-    public UserDatabase(String fileLocation) {}
+    public UserDatabase(String fileLocation) {
+        this.fileLocation = fileLocation;
+        this.users        = new ArrayList<>();
+    }
+
+    public List<User> getAllUsers() { return new ArrayList<>(users); }
+
+    public boolean userExists(String username) {
+        if (username == null || username.isBlank()) return false;
+        return getUser(username) != null;
+    }
 
     public void loadUsers() {}
     public void saveUsers() {}
-
     public User getUser(String username) {}
-    public List<User> getAllUsers() {}
-
-    public boolean userExists(String username) {}
     public boolean createUser(String username, String passwordHash) {}
     public boolean deleteUser(String username) {}
-
     public boolean authenticate(String username, String passwordHash) {}
 }
