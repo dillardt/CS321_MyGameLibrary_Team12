fd06c8ebc58fc982743a71a8868aea13c22e2f20
Srijal <dongolsrijal@gmail.com>
2026-04-19 15:32:50 -0500

    Model: Review class: Constructor and some methods

--- src/Model/Review.java
+++ src/Model/Review.java
@@ -4,15 +4,32 @@ public class Review {
 
     private int rating;
     private String comment;
-    private User author;
-    private Game game;
+    private final User author;
+    private final Game game;
 
-    public Review(int rating, String comment, User author, Game game) {}
+    public Review(int rating, String comment, User author, Game game) {
+        if (rating < 1) {
+            this.rating = 1;
+        } else if (rating > 10) {
+            this.rating = 10;
+        } else {
+            this.rating = rating;
+        }
 
-    public int getRating() {}
-    public String getComment() {}
-    public User getAuthor() {}
-    public Game getGame() {}
+        if (comment == null || comment.isBlank()) {
+            this.comment = "No comment provided.";
+        } else {
+            this.comment = comment;
+        }
+
+        this.author = author;
+        this.game   = game;
+    }
+
+    public int getRating() { return rating; }
+    public String getComment() { return comment; }
+    public User getAuthor() { return author; }
+    public Game getGame() { return game; }
 
     public boolean editReview(String newComment, int newRating) {}
     public String getSummary() {}
