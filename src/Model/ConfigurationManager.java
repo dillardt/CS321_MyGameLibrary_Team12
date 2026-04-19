e6f731e519627be384a2c8f1f521b2d0e7829ada
Srijal <dongolsrijal@gmail.com>
2026-04-19 18:10:23 -0500

    Model: ConfigurationManager class: Skeleton

--- src/Model/ConfigurationManager.java
+++ src/Model/ConfigurationManager.java
@@ -2,18 +2,30 @@ package Model;
 
 public class ConfigurationManager {
 
+    private static final String KEY_GAME_DB = "gameDBPath";
+    private static final String KEY_USER_DB = "userDBPath";
+
     private String configFilePath;
     private String gameDBPath;
     private String userDBPath;
 
     public ConfigurationManager(String configFilePath) {
-
     }
 
-    public void loadConfiguration() { }
-    public void saveConfiguration() { }
+    public void loadConfiguration() {
+    }
+
+    public void saveConfiguration() {
+    }
+
     public String getGameDBPath() { return null; }
+
     public String getUserDBPath() { return null; }
-    public void setGameDBPath(String path) { }
-    public void setUserDBPath(String path) { }
+
+    public String getConfigFilePath() { return null; }
+
+    public boolean isValid() { return false; }
+
+    @Override
+    public String toString() { return null; }
 }
