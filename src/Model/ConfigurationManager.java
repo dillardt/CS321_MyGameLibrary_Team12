c48f83944dfdbb22105093011dec018f6209d2ff
Srijal <dongolsrijal@gmail.com>
2026-04-19 18:10:54 -0500

    Model: ConfigurationManager class: Constructor and some methods

--- src/Model/ConfigurationManager.java
+++ src/Model/ConfigurationManager.java
@@ -5,11 +5,14 @@ public class ConfigurationManager {
     private static final String KEY_GAME_DB = "gameDBPath";
     private static final String KEY_USER_DB = "userDBPath";
 
-    private String configFilePath;
+    private final String configFilePath;
     private String gameDBPath;
     private String userDBPath;
 
     public ConfigurationManager(String configFilePath) {
+        this.configFilePath = configFilePath;
+        this.gameDBPath = null;
+        this.userDBPath = null;
     }
 
     public void loadConfiguration() {
@@ -18,14 +21,28 @@ public class ConfigurationManager {
     public void saveConfiguration() {
     }
 
-    public String getGameDBPath() { return null; }
+    public String getGameDBPath() {
+        return gameDBPath;
+    }
 
-    public String getUserDBPath() { return null; }
+    public String getUserDBPath() {
+        return userDBPath;
+    }
 
-    public String getConfigFilePath() { return null; }
+    public String getConfigFilePath() {
+        return configFilePath;
+    }
 
-    public boolean isValid() { return false; }
+    public boolean isValid() {
+        return gameDBPath != null && !gameDBPath.isBlank()
+                && userDBPath != null && !userDBPath.isBlank();
+    }
 
     @Override
-    public String toString() { return null; }
+    public String toString() {
+        return "ConfigurationManager | Config: " + configFilePath
+                + " | GameDB: " + gameDBPath
+                + " | UserDB: " + userDBPath
+                + " | Valid: " + isValid();
+    }
 }
