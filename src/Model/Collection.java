2b227518b961cc667cfd8884f80b48fdbea3797b
Srijal <dongolsrijal@gmail.com>
2026-04-19 15:23:31 -0500

    Model: Collection class: Constructor and some methods

--- src/Model/Collection.java
+++ src/Model/Collection.java
@@ -8,18 +8,30 @@ public class Collection {
     private String name;
     private List<Game> games;
 
-    public Collection(String name) {}
+    public Collection(String name) {
+        this.name  = name;
+        this.games = new ArrayList<>();
+    }
 
-    public String getName() {}
-    public void setName(String name) {}
-    public int getSize() {}
-    public List<Game> getGames() {}
+    public String getName() { return name; }
+
+    public void setName(String name) { this.name = name; }
+
+    public int getSize() { return games.size(); }
+
+    public List<Game> getGames() { return new ArrayList<>(games); }
+
+    public boolean containsGame(Game game) {
+        if (game == null) return false;
+        return games.contains(game);
+    }
+
+    @Override
+    public String toString() {
+        return name + " (" + games.size() + " games)";
+    }
 
     public boolean addGame(Game game) {}
     public boolean removeGame(Game game) {}
-    public boolean containsGame(Game game) {}
     public Game getGameById(String gameID) {}
-
-    @Override
-    public String toString() {}
 }
