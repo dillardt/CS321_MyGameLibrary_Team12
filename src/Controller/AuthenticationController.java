2684f1f0abfec77066537e37a8ab281af7554417
Srijal <dongolsrijal@gmail.com>
2026-04-19 22:37:43 -0500

    Model: AuthenticationController class: Skeleton

--- src/Controller/AuthenticationController.java
+++ src/Controller/AuthenticationController.java
@@ -5,16 +5,35 @@ import Model.UserDatabase;
 
 public class AuthenticationController {
 
-    private UserDatabase userDatabase;
+    private final UserDatabase userDB;
     private User currentUser;
 
-    public AuthenticationController(UserDatabase userDatabase) {
-
+    public AuthenticationController(UserDatabase userDB) {
+        this.userDB = userDB;
+        this.currentUser = null;
     }
 
-    public boolean login(String username, String password) { return false; }
-    public void logout() { }
-    public boolean createAccount(String username, String password) { return false; }
-    public User getCurrentUser() { return null; }
-    public boolean isLoggedIn() { return false; }
+    public User getCurrentUser() {
+        return null;
+    }
+
+    public boolean isLoggedIn() {
+        return false;
+    }
+
+    public boolean login(String username, String passwordHash) {
+        return false;
+    }
+
+    public void logout() {
+    }
+
+    public boolean register(String username, String passwordHash) {
+        return false;
+    }
+
+    public String toString() {
+        return "AuthenticationController";
+    }
+
 }
